b1505bc27d0cdad86748ee82d6d3e82ea2f5a3f4
jh1992jh <janihanninen@hotmail.com>
2018-11-12 20:35:15 +0200

    Updates

---
+++ precache-manifest.25260997a7cd519fd720b6e47363eee6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "6201f544613cd4f28975d05128667335",
+    "url": "/jh-dev/static/media/sm-mobile-1.6201f544.png"
+  },
+  {
+    "revision": "5baf7c472184e2736bbe",
+    "url": "/jh-dev/static/css/main.f9bd943b.chunk.css"
+  },
+  {
+    "revision": "34a1456b27f4decf0965",
+    "url": "/jh-dev/static/js/1.34a1456b.chunk.js"
+  },
+  {
+    "revision": "bb4fc1df3215b3da6396",
+    "url": "/jh-dev/static/js/runtime~main.bb4fc1df.js"
+  },
+  {
+    "revision": "15712763a14383053fe155154e50dd01",
+    "url": "/jh-dev/static/media/reduxLogo.15712763.svg"
+  },
+  {
+    "revision": "1541ec306ee0ef8d3308b631c9d74223",
+    "url": "/jh-dev/static/media/sf-desktop-2.1541ec30.png"
+  },
+  {
+    "revision": "9345cf8bbf420cc225a399c51d853312",
+    "url": "/jh-dev/static/media/sf-mobile-1.9345cf8b.png"
+  },
+  {
+    "revision": "650d0d0feb12089905574a8db1e46ab5",
+    "url": "/jh-dev/static/media/sf-mobile-2.650d0d0f.png"
+  },
+  {
+    "revision": "21459cedc85d1139a08d2f55f54fab40",
+    "url": "/jh-dev/static/media/sm-desktop-1.21459ced.png"
+  },
+  {
+    "revision": "5baf7c472184e2736bbe",
+    "url": "/jh-dev/static/js/main.5baf7c47.chunk.js"
+  },
+  {
+    "revision": "47b5ee388c4d63903987e6bb2570130c",
+    "url": "/jh-dev/static/media/sm-mobile-2.47b5ee38.png"
+  },
+  {
+    "revision": "0ec1b4154578047c896a8a74d7862d80",
+    "url": "/jh-dev/static/media/fm-desktop-1.0ec1b415.png"
+  },
+  {
+    "revision": "c1ffc4bac2abbb06bcde6b9559408927",
+    "url": "/jh-dev/static/media/fm-mobile-1.c1ffc4ba.png"
+  },
+  {
+    "revision": "5081941396f34ed89fd93278a11aa521",
+    "url": "/jh-dev/static/media/fm-mobile-2.50819413.png"
+  },
+  {
+    "revision": "7a00c9d8f8deecafe9cc1c3f0123516e",
+    "url": "/jh-dev/static/media/rlp-desktop-1.7a00c9d8.png"
+  },
+  {
+    "revision": "129ace3e5f2b28ebcf14ba41e56d2e2c",
+    "url": "/jh-dev/static/media/rlp-mobile-1.129ace3e.png"
+  },
+  {
+    "revision": "560aa8ca9ed789df3ddf4e9643993159",
+    "url": "/jh-dev/static/media/rlp-mobile-2.560aa8ca.png"
+  },
+  {
+    "revision": "6536af44afe3a0f1b2eb99980861c907",
+    "url": "/jh-dev/static/media/myownpic.6536af44.jpg"
+  },
+  {
+    "revision": "5bea2e588ee772a10ea8ec96fed9b786",
+    "url": "/jh-dev/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/jh-dev/precache-manifest.25260997a7cd519fd720b6e47363eee6.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/jh-dev/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
